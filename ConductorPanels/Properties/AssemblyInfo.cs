efabc20fc6b75bb4a95af119ad73e8335bd83d36
Vladimir1965 <vladimir_ladma@seznam.cz>
2021-09-10 00:36:29 +0200

    Add project properties.

---
+++ ConductorPanels/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Traced-Ideas, Czech republic">
+// Copyright (c) 1990-2021 All Right Reserved
+// </copyright>
+// <author>vl</author>
+// <email></email>
+// <date>2021-09-01</date>
+// <summary>Part of Largo Composer</summary>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+//// Obecné informace o sestavení se řídí přes následující 
+//// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+//// přidružené k sestavení.
+[assembly: AssemblyTitle("ConductorPanels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConductorPanels")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+//// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+//// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+//// modelu COM, nastavte atribut ComVisible daného typu na hodnotu True.
+[assembly: ComVisible(false)]
+
+//// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("ae2364cd-4ad4-4254-8583-d9d52a0bab60")]
+
+//// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+////
+////    Hlavní verze
+////    Podverze
+////    Číslo sestavení
+////    Revize
+////
+//// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+//// pomocí zástupného znaku * takto:
+//// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
