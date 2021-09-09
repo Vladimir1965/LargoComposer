995c55d074abcebec3ab6dccf55397e8e025d436
Vladimir1965 <vladimir_ladma@seznam.cz>
2021-09-09 23:55:43 +0200

    Add Manager.

---
+++ ManagerPanels/ManagerSettings.cs
@@ -0,0 +1,201 @@
+﻿// <copyright file="ManagerSettings.cs" company="Traced-Ideas, Czech republic">
+// Copyright (c) 1990-2021 All Right Reserved
+// </copyright>
+// <author>vl</author>
+// <email></email>
+// <date>2021-09-01</date>
+// <summary>Part of Largo Composer</summary>
+
+using System;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.IO;
+using System.Xml.Linq;
+using LargoSharedClasses.Abstract;
+using LargoSharedClasses.Settings;
+
+namespace ManagerPanels
+{
+    /// <summary>
+    /// Musical Settings.
+    /// </summary>
+    public class ManagerSettings
+    {
+        #region Fields
+        /// <summary>
+        /// Singleton variable.
+        /// </summary>
+        private static ManagerSettings internalSingleton = new ManagerSettings();
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ManagerSettings"/> class.
+        /// </summary>
+        public ManagerSettings() {
+            this.SettingsImport = new SettingsImport();
+            this.SkipNegligibleLines = true;
+            this.SaveInternalTemplates = true;
+            this.PathToInternalTemplates = MusicalSettings.Singleton.Folders.GetFolder(MusicalFolder.InternalTemplates);
+            var folder = MusicalSettings.Singleton.Folders.GetFolder(MusicalFolder.InternalSettings);
+            var path = Path.Combine(folder, "ManagerSettings.xml");
+            this.PathSettings = path;
+        }
+
+        #endregion
+
+        #region Static properties
+        /// <summary>
+        /// Gets or sets the ProcessLogger Singleton.
+        /// </summary>
+        /// <value>
+        /// Property description.
+        /// </value>
+        /// <exception cref="System.InvalidOperationException">Singleton MusicalSettings is null.</exception>
+        public static ManagerSettings Singleton {
+            get {
+                Contract.Ensures(Contract.Result<ManagerSettings>() != null);
+                if (internalSingleton == null) {
+                    throw new InvalidOperationException("Singleton ManagerSettings is null.");
+                }
+
+                return internalSingleton;
+            }
+
+            set => internalSingleton = value;
+        }
+        #endregion
+
+        #region General Properties
+        /// <summary>
+        /// Gets or sets the folders.
+        /// </summary>
+        /// <value>
+        /// The folders.
+        /// </value>
+        public SettingsFolders Folders { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path settings.
+        /// </summary>
+        /// <value>
+        /// The path settings.
+        /// </value>
+        public string PathSettings { get; set; }
+
+        /// <summary>
+        /// Gets or sets CultureInfo.
+        /// </summary>
+        /// <value> General musical property.</value>
+        public CultureInfo CultureInfo { get; set; }
+        #endregion
+
+        #region Public properties - settings sessions
+
+        /// <summary>
+        /// Gets or sets the settings import.
+        /// </summary>
+        /// <value>
+        /// The settings import.
+        /// </value>
+        public SettingsImport SettingsImport { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [skip negligible lines].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [skip negligible lines]; otherwise, <c>false</c>.
+        /// </value>
+        public bool SkipNegligibleLines { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [save internal templates].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [save internal templates]; otherwise, <c>false</c>.
+        /// </value>
+        public bool SaveInternalTemplates { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path to internal templates.
+        /// </summary>
+        /// <value>
+        /// The path to internal templates.
+        /// </value>
+        public string PathToInternalTemplates { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path to music files.
+        /// </summary>
+        /// <value>
+        /// The path to music files.
+        /// </value>
+        public string PathToMusicFiles { get; set; }
+        #endregion
+
+        #region MusicalSettings - Load/Save
+        /// <summary>
+        /// Loads the musical settings.
+        /// </summary>
+        public void Load() {
+            var path = this.PathSettings;
+            var root = XmlSupport.GetXDocRoot(path);
+            if (root == null || root.Name != "ManagerSettings") {
+                return;
+            }
+
+            var xsetup = root;
+            this.Read(xsetup);
+        }
+
+        /// <summary>
+        /// Saves the musical settings.
+        /// </summary>
+        public void Save() {
+            var xsetup = this.Write();
+            var xdoc = new XDocument(new XDeclaration("1.0", "utf-8", null), xsetup);
+            xdoc.Save(this.PathSettings);
+        }
+
+        /// <summary>
+        /// Reads the musical settings.
+        /// </summary>
+        /// <param name="markSettings">The mark settings.</param>
+        public void Read(XContainer markSettings) { //// XElement
+            Contract.Requires(markSettings != null);
+            if (markSettings == null) {
+                return;
+            }
+
+            XElement ximport = markSettings.Element("Import");
+
+            ////  Import
+            this.SettingsImport.SetXElement(ximport);
+        }
+
+        /// <summary>
+        /// Writes the musical setup.
+        /// </summary>
+        /// <returns>
+        /// Returns value.
+        /// </returns>
+        public XElement Write() {
+            XElement xsetup = new XElement("ManagerSettings", null);
+
+            //// Program settings, musical limit
+            XElement ximport = this.SettingsImport.GetXElement;
+            xsetup.Add(ximport);
+
+            return xsetup;
+        }
+        #endregion
+
+        #region String representation
+        /// <summary> String representation of the object. </summary>
+        /// <returns> Returns value. </returns>
+        public override string ToString() {
+            return "ManagerSettings";
+        }
+        #endregion
+    }
+}
---
+++ ManagerPanels/MaterialHarmonicModality.xaml.cs
@@ -0,0 +1,186 @@
+﻿// <copyright file="MaterialHarmonicModality.xaml.cs" company="Traced-Ideas, Czech republic">
+// Copyright (c) 1990-2021 All Right Reserved
+// </copyright>
+// <author>vl</author>
+// <email></email>
+// <date>2021-09-01</date>
+// <summary>Part of Largo Composer</summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using LargoSharedClasses.Abstract;
+using LargoSharedClasses.Music;
+using LargoSharedClasses.Support;
+
+namespace ManagerPanels
+{
+    /// <summary>
+    /// PanelHarmonic Motive.
+    /// </summary>
+    public sealed partial class MaterialHarmonicModality
+    {
+        #region Fields
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MaterialHarmonicModality"/> class.
+        /// </summary>
+        public MaterialHarmonicModality() {
+            this.InitializeComponent();
+        }
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the drag start point.
+        /// </summary>
+        /// <value>
+        /// The drag start point.
+        /// </value>
+        public Point DragStartPoint { get; set; }
+
+        /// <summary> Gets or sets a list of ordered. </summary>
+        /// <value> A list of ordered. </value>
+        public List<HarmonicModality> List { get; set; }
+
+        /// <summary>
+        /// Gets or sets the filtered list.
+        /// </summary>
+        /// <value>
+        /// The filtered list.
+        /// </value>
+        public List<HarmonicModality> ResultList { get; set; }
+
+        #endregion
+
+        /// <summary> Loads the data. </summary>
+        public override void LoadData() {
+            base.LoadData();
+            var system = HarmonicSystem.GetHarmonicSystem(DefaultValue.HarmonicOrder);
+            var structures = PortCatalogs.Singleton.HarmonicEssence;
+            if (structures == null) {
+                return;
+            }
+
+            var modalities = (from ms in structures where ms.ModalityName.Length > 0 && ms.Level >= 5 && ms.Level <= 7 select ms).ToList();
+            var list = new List<HarmonicModality>();
+            foreach (var modality in modalities) {
+                var hs = new HarmonicModality(system, modality.StructuralCode) {
+                    Name = modality.ModalityName,
+                    //// Shortcut = !string.IsNullOrWhiteSpace(chord.Shortcut) ? chord.Shortcut : "X",                    
+                    ClassCode = modality.ClassNumber.ToString(),
+                    Number = modality.Number,
+                    DecimalNumber = modality.Number
+                };
+                list.Add(hs);
+            }
+
+            this.List = list.OrderBy(x => x.Level).ThenBy(x => x.ToneSchema).ToList();
+            this.ResultList = this.List;
+
+            //// var ordered = list.OrderBy(x => x.ClassCode + x.Shortcut);
+            this.DataGridMaterial.ItemsSource = null;
+            this.DataGridMaterial.ItemsSource = this.ResultList;
+            this.DataGridMaterial.Items.Refresh();
+        }
+
+        /// <summary> Filter by modality. </summary>
+        /// <param name="givenModality"> The given modality. </param>
+        public void FilterByModality(HarmonicModality givenModality) {
+            if (this.List == null) {
+                return;
+            }
+
+            var resultList = new List<HarmonicModality>();
+            foreach (var hs in this.List) {
+                bool covered = true;
+                for (byte j = 0; j < givenModality.GSystem.Order; j++) {
+                    if (hs.IsOn(j) && givenModality.IsOff(j)) {
+                        covered = false;
+                        break;
+                    }
+                }
+
+                if (covered) {
+                    resultList.Add(hs);
+                }
+            }
+
+            this.ResultList = resultList;
+            this.DataGridMaterial.ItemsSource = null;
+            this.DataGridMaterial.ItemsSource = resultList;
+            this.DataGridMaterial.Items.Refresh();
+        }
+
+        #region Harmonic Modalities
+
+        /// <summary>
+        /// Selected modality.
+        /// </summary>
+        /// <returns> Returns value. </returns>
+        public HarmonicModality SelectedModality() {
+            var item = this.DataGridMaterial.SelectedItem as HarmonicModality;
+            if (item == null) {
+                return null;
+            }
+
+            return item;
+        }
+
+        /// <summary>
+        /// Handles the Selection Changed event of the Data Grid Harmonic Bars control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.Controls.SelectionChangedEventArgs"/> instance containing the event data.</param>
+        private void DataGridHarBars_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            e.Handled = true;
+        }
+        #endregion
+
+        #region Drag-drop
+        /// <summary>
+        /// Handles the PreviewMouseLeftButtonDown event of the List control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="MouseButtonEventArgs"/> instance containing the event data.</param>
+        private void List_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            var harmonicModality = this.DataGridMaterial.SelectedItem as HarmonicModality;
+            if (harmonicModality == null) {
+                return;
+            }
+
+            if (!(e.Source is Image image)) {
+                return;
+            }
+
+            // Store the mouse position
+            // Initialize the drag & drop operation
+            this.DragStartPoint = e.GetPosition(null);
+            DataObject data = new DataObject("HarmonicModality", harmonicModality);
+            DragDrop.DoDragDrop(image, data, DragDropEffects.All);
+        }
+
+        /// <summary>
+        /// Handles the MouseMove event of the List control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+        private void List_MouseMove(object sender, MouseEventArgs e) {
+            // Get the current mouse position
+            Point mousePos = e.GetPosition(null);
+            Vector diff = this.DragStartPoint - mousePos;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                 Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance)) {
+            }
+        }
+        #endregion
+    }
+}
